1a4c004e74365988ad3bd1c9426becc9753e6a3d
Sardeer Azim <140551311+itsazim63@users.noreply.github.com>
2024-05-15 01:48:59 +0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let sections = document.querySelectorAll('section');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -600;
+        let offset = sec.offsetTop -700;
         let height = sec.offsetHeight;
 
         if(top >= offset && top < offset + height){
